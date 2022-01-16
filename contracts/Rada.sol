fa550c95649f62303d21e91b5a76017a71f6036f
kv0311 <kvxi98@gmail.com>
2022-01-16 15:38:56 +0700

    upte

--- contracts/Rada.sol
+++ contracts/Rada.sol
@@ -3,5 +3,28 @@ import "./ERC721.sol";
 
 contract Rada is ERC712{
     string public name;
-    string 
+    string public symbol;
+    uint256 public tokenCount;
+    mapping(uint256 => string) private _tokenURIs;
+    constructor(string memory _name, string memory _symbol){
+        name =_name;
+        symbol =_symbol;
+    }
+
+    function tokenURI(uint256 tokenID) public view returns (string memory){
+            require(_owners[tokenID] != address(0),"token id is invalid");
+            return _tokenURIs[tokenID];
+    }
+    function mint(string memory _tokenURI) public {
+        tokenCount +=1;
+        _balances[msg.sender] = 1;
+        _owners[tokenCount] = msg.sender;
+        _tokenURIs[tokenCount] = _tokenURI;
+
+        emit Transfer(address(0),  msg.sender, tokenCount);
+    }
+
+     function supportInterface(bytes4 interfaceID) public pure override returns (bool){
+        return interfaceID == 0x80ac58cd || interfaceID == 0x5b5e139f;
+    }
 }
