dd478ec8b38b3718aa5c81424149bb86cee90ce8
kv0311 <kvxi98@gmail.com>
2022-01-25 09:55:01 +0700

    update

--- scripts/deploy-draw.js
+++ scripts/deploy-draw.js
@@ -1,35 +1,40 @@
-const {ethers} = require('hardhat');
-async function main(){
-    try{
-      // const MultiPartRLEToSVG = await ethers.getContractFactory("MultiPartRLEToSVG");
-      // const multiPartRLEToSVG =await MultiPartRLEToSVG.deploy();
-      // const multiPartRLEToSVGResult = await MultiPartRLEToSVG.deployed();
-      
-      const SVGNFT =  await ethers.getContractFactory("NFTManagement", {
-        libraries: {
-          NFTUtils: "0x76dafa79a44215344dc132f6fc6364071299994a",
-          MultiPartRLEToSVG: "0xde86f7a6e6b97bfe857065e3415ea479de69b9d7"
-        },
-      });
-      const svgNFT =await SVGNFT.deploy();
-      let multiPartRLEToSVGResult = await svgNFT.deployed();
-    
-        // const str1 = await svgNFT.create("123");
-        // const str2 = await svgNFT.tokenURI(0);
-        // console.log("NFT successfully mint")
-       console.log(multiPartRLEToSVGResult)
-    } catch (err){
-        console.log(err)
-    }
-    
-}
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
 
+      // const SVGNFT =  await ethers.getContractFactory("NFTManagement", {
+      //   libraries: {
+      //     NFTUtils: "0x76dafa79a44215344dc132f6fc6364071299994a",
+      //     MultiPartRLEToSVG: "0xde86f7a6e6b97bfe857065e3415ea479de69b9d7"
+      //   },
+      // });
+      
+
+  async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const BUSDToken = await ethers.getContractFactory("Descriptor",{
+      libraries: {
+        MultiPartRLEToSVG: "0xde86f7a6e6b97bfe857065e3415ea479de69b9d7"
+      }
+    });
+    // Deploy 100M token
+    const contractDeploy = await BUSDToken.deploy();
+  
+    const deployContract = await contractDeploy.deployed();
+  
+    console.log("Contract deployed to:", deployContract);
+  
+    // console.log("BUSD Token address:", contractDeploy.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
 
 
   
